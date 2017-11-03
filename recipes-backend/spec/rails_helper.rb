2011cee44b37f34995d1b7940e3e6d4ecb1ff5e0
Anson <leungans@gmail.com>
2017-11-02 17:43:11 -0700

    fix gems for testing

--- recipes-backend/spec/rails_helper.rb
+++ recipes-backend/spec/rails_helper.rb
@@ -1,8 +1,15 @@
-# require database cleaner at the top level
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
 require 'database_cleaner'
+require 'factory_girl'
 
-# [...]
-# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+ActiveRecord::Migration.maintain_test_schema!
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
@@ -10,23 +17,22 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-# [...]
-RSpec.configuration do |config|
-  # [...]
-  # add `FactoryGirl` methods
-  config.include FactoryGirl::Syntax::Methods
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+RSpec.configure do |config|
+
+  config.include FactoryGirl::Syntax::Methods
+  config.include RequestSpecHelper, type: :request
+
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
   end
 
-  # start the transaction strategy as examples are run
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
-  # [...]
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
 end
