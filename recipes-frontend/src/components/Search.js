6258088a0c0b4baa5fad10853dea7524c5da1d32
Anson <leungans@gmail.com>
2017-11-04 01:06:40 -0700

    center page

--- recipes-frontend/src/components/Search.js
+++ recipes-frontend/src/components/Search.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import '../App.css';
+import '../App.css';
 import '../Search.css';
 import axios from 'axios';
 
@@ -19,13 +19,8 @@ class Search extends Component {
   getRecipes(searchParams) {
     var self = this;
 
-    console.log(`searching for: ${searchParams}`)
     axios.get(`https://api.edamam.com/search?q=${searchParams}`)
     .then(function (response) {
-      console.log(response.data.hits);
-      // loop thru response.data.hits add each recipe obj to this.state.recipes
-      // get name, ingredients, recipe_url, image_url, ingredient_count
-
       self.setState({ recipes: response.data.hits })
     })
     .catch(function (error) {
@@ -61,54 +56,28 @@ class Search extends Component {
 
   render() {
     return (
-      <div>
+      <div className='App'>
         <form onSubmit={this.handleSubmit}>
-          <label>
-            Enter ingredients:
+          <h1>Enter Ingredients:</h1>
             <input type="text" value={this.state.searchParams} onChange={this.handleChange} />
-          </label>
           <input type="submit" value="Submit" />
         </form>
-        <div>
-        {this.state.recipes.map((recipeData, index) => {
-          return (
-            <div
-            key={recipeData.recipe.url}>
-              <p>{recipeData.recipe.label} : {recipeData.recipe.ingredients.length}</p>
-            </div>
-          )
-        })}
-        </div>
-          <hr />
-          Ordered recipes:
-          <div className='recipe-container'>
+          <div>
             {this.orderedRecipes(this.state.recipes).map((recipeData, index) => {
               return (
-                <div style={{ backgroundImage: `url(${recipeData.image})` }} className='recipe-item'>
-                  <a style={{ color: 'white', textDecoration: "none" }}href={recipeData.url}>{recipeData.name} : {recipeData.count}</a>
+                <div className='recipe-divider'>
+                  <div className='recipe-item' style={{backgroundImage: `url(${recipeData.image})`}} >
+                    <a href={recipeData.url}>
+                      {recipeData.name} : {recipeData.count}
+                    </a>
+                  </div>
                 </div>
-              )
-            })}
+                )
+              })}
           </div>
       </div>
     );
   }
 }
 
-const styles = {
-  recipeList: {
-    // flex: 1,
-    // backgroundColor: 'aliceblue'
-    // textAlign: 'center',
-    // justifyContent: 'center'
-  },
-  recipeItem: {
-    // width: 400,
-    // height: 100,
-    // margin: 10,
-    // textAlign: 'center'
-  }
-};
-
 export default Search;
-// { backgroundImage: `url(${recipeData.image})` }
